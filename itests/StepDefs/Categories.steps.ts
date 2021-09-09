b174a8130949e3384da465800f0c193ab526551c
Prateek Chauhan <68464629+PrateekChauhan0194@users.noreply.github.com>
2021-09-10 01:52:33 +1000

    Update Categories.steps.ts

--- itests/StepDefs/Categories.steps.ts
+++ itests/StepDefs/Categories.steps.ts
@@ -2,11 +2,11 @@ import { Given, Then } from 'cucumber';
 import { Categories } from '../Methods/Categories';
 
 Given(/^I am on categories page$/,
-    async (t: TestController, []: string[]) => {
+    async (t: TestController) => {
         await Categories.goToCategories(t);
 });
 
 Then(/^I validate that the main container for categories page is present$/,
-    async (t: TestController, []: string[]) => {
+    async (t: TestController) => {
         await Categories.validatePresenceOfMainContainer(t);
 });
